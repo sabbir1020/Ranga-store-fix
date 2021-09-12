00291aa6c235d89a294447da2830398478010c33
sabbir1020 <gazisabbir924@gmail.com>
2021-09-13 05:40:30 +0600

    img and css added

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then(response => response.json())
+    .then(data => showProducts(data));
+    // console.log(data)
 };
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  // console.log(products);
   const allProducts = products.map((pd) => pd);
+  // console.log(allProducts)
   for (const product of allProducts) {
+    // console.log(product);
     const image = product.images;
+    console.log(image)
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image"  src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h4> count:${product.rating.count}</h4>
+      <p> Rateing:${product.rating.rate}<p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
