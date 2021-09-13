54786ce5fbdb1c88f381f3e13fa6e64e0a5817ba
sabbir1020 <gazisabbir924@gmail.com>
2021-09-13 14:48:31 +0600

    css and js files added

--- js/app.js
+++ js/app.js
@@ -18,17 +18,19 @@ const showProducts = (products) => {
     console.log(image)
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image"  src=${product.image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h4> count:${product.rating.count}</h4>
-      <p> Rateing:${product.rating.rate}<p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+    div.innerHTML = 
+     ` <div class="single-product">
+          <div>
+            <img class="product-image"  src=${product.image}></img>
+          </div>
+          <h3>${product.title}</h3>
+          <p>Category: ${product.category}</p>
+          <h4> count:${product.rating.count}</h4>
+          <p> Rateing:${product.rating.rate}<p>
+          <h2>Price: $ ${product.price}</h2>
+          <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+          <button id="details-btn" class="btn btn-danger">Details</button>
+       </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
